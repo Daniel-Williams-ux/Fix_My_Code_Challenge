132dd1c0d2551b1d3437699fd0eccd1b507c26e8
Daniel Williams <williamsoligodaniel@gmail.com>
2022-09-18 19:08:31 +0100

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 """ FizzBuzz
 """
-
 import sys
 
 
@@ -33,13 +31,8 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")                                                                        
-        sys.exit(1)                                                                                                 
-    try:                                                                                                            
-        number = int(sys.argv[1])                                                                                   
-        fizzbuzz(number)                                                                                            
-    except ValueError:                                                                                              
-        print("Input is not a number")                                                                              
-        print("Usage: ./0-fizzbuzz.py <number>")                                                                    
-        print("Example: ./0-fizzbuzz.py 89")                                                                        
-        sys.exit(1) 
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
